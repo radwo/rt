d2c29d076d3938cc350d6bb9f479ecbe48b15a63
Radosław Woźniak <rad.wozniak@gmail.com>
2018-01-27 15:57:54 +0100

    implement template on task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
   validates :name, uniqueness: true, presence: true, length: { maximum: 255 }
   validates :description, presence: true, length: { maximum: 1500 }
+  validates :template, presence: true, length: { maximum: 1500 }
 
   has_many :solutions, inverse_of: :task, dependent: :delete_all
   has_many :attempts, inverse_of: :task, dependent: :restrict_with_error
